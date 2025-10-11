f7b79c4c7144096cdc800aef92c22016c2e1972e
Derlis Caballero Mendoza <derliscaballero91@gmail.com>
2025-10-11 12:18:13 -0300

    creacion de requeriments

--- Clase6/app.py
+++ Clase6/app.py
@@ -9,4 +9,4 @@ app.register_blueprint(login) #registramos el blueprint
 def home():
     return "Hola, Unida Experto en Python!"
 if __name__ == '__main__':
-    app.run(debug=True,port=5000,host='0.0.0.0')
+    app.run(debug=True,port=5000,host='0.0.0.0')#Favor colocar la IP de su maquina local
